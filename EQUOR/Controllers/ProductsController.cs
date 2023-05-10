4532169ea129a170921a2e6d13e426f070da90d6
WALTER SALAZAR <wasalazar@ucundinamarca.edu.co>
2023-05-09 21:34:20 -0500

    Calculo de huella implementado, faltan controladores y vistas

---
+++ EQUOR/Controllers/ProductsController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EQUOR.DataContext;
+using EQUOR.Models;
+using Microsoft.CodeAnalysis;
+
+namespace EQUOR.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly DataDBContext _context;
+
+        public ProductsController(DataDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index(int productId)
+        {
+            // Buscar el producto en la base de datos
+            var product = _context.Products.Find(productId);
+
+            // Calcular la huella de carbono del producto
+            var carbonFootprint = product.CalculateCarbonFootprint(product.TipeTransport,
+                                                           product.QWaterUsed,
+                                                           product.QEnergy,
+                                                           product.QWaste);
+
+            // Actualizar el valor de la huella de carbono en la base de datos
+            product.CarbonFootprint = carbonFootprint;
+            _context.SaveChanges();
+
+            // Retornar la huella de carbono calculada
+            return Ok(carbonFootprint);
+            var dataDBContext = _context.Products.Include(p => p.Manager);
+            return View(await dataDBContext.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Manager)
+                .FirstOrDefaultAsync(m => m.IdProduct == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+       
+
+
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["IdManager"] = new SelectList(_context.Set<Manager>(), "IdManager", "IdManager");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdProduct,IdManager,Name,DesProduct,TipeTransport,QWaterUsed,QEnergy,QWaste,CodigoQR,CarbonFootprint,TimeSearch")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdManager"] = new SelectList(_context.Set<Manager>(), "IdManager", "IdManager", product.IdManager);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdManager"] = new SelectList(_context.Set<Manager>(), "IdManager", "IdManager", product.IdManager);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdProduct,IdManager,Name,DesProduct,TipeTransport,QWaterUsed,QEnergy,QWaste,CodigoQR,CarbonFootprint,TimeSearch")] Product product)
+        {
+            if (id != product.IdProduct)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.IdProduct))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdManager"] = new SelectList(_context.Set<Manager>(), "IdManager", "IdManager", product.IdManager);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Manager)
+                .FirstOrDefaultAsync(m => m.IdProduct == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'DataDBContext.Products'  is null.");
+            }
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+          return (_context.Products?.Any(e => e.IdProduct == id)).GetValueOrDefault();
+        }
+    }
+}
--- EQUOR/Models/Product.cs
+++ EQUOR/Models/Product.cs
@@ -16,10 +16,42 @@ namespace EQUOR.Models
         public int QEnergy { get; set; }
         public int QWaste { get; set; }
         public string CodigoQR { get; set; }
-        public int CarbonFootprint { get; set; }
+        public double CarbonFootprint { get; set; }
         public int TimeSearch { get; set; }
 
         [ForeignKey("IdManager")]
         public Manager Manager { get; set; }
+        public double CalculateCarbonFootprint(string tipeTransport, int qWaterUsed, int qEnergy, int qWaste)
+        {
+            double huellaCarbono = 0;
+
+            // Cálculo de la huella de carbono según el tipo de transporte utilizado
+            switch (tipeTransport)
+            {
+                case "Avión":
+                    huellaCarbono += 200; // Supongamos que un vuelo de 1000 km emite 200 kg de CO2 por pasajero
+                    break;
+                case "Automóvil":
+                    huellaCarbono += 120; // Supongamos que un coche emite 120 g de CO2 por km recorrido
+                    break;
+                case "Tren":
+                    huellaCarbono += 40; // Supongamos que un tren emite 40 g de CO2 por km recorrido
+                    break;
+                case "Bicicleta":
+                    huellaCarbono += 0; // La huella de carbono de la bicicleta se considera nula
+                    break;
+                default:
+                    huellaCarbono += 0; // Si el tipo de transporte no está especificado, se considera una huella de carbono nula
+                    break;
+            }
+
+            // Cálculo de la huella de carbono según el consumo de agua y energía y la generación de residuos
+            huellaCarbono += qWaterUsed * 5; // Supongamos que se emiten 5 kg de CO2 por cada metro cúbico de agua utilizada
+            huellaCarbono += qEnergy * 0.5; // Supongamos que se emiten 0.5 kg de CO2 por cada kW/h de energía consumido
+            huellaCarbono += qWaste * 50; // Supongamos que se emiten 50 kg de CO2 por cada tonelada de residuos generada
+
+            return huellaCarbono;
+        }
+
     }
 }
