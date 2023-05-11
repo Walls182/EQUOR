b89c951ac3c743b4d2e26b7f504ffae96c6ec0b1
GEORGE STEVEN RUBIO GAITAN <gsrubio@ucundinamarca.edu.co>
2023-05-10 20:18:41 -0500

    Logica del Login

---
+++ EQUOR/Logica_login/Logica_Login.cs
@@ -0,0 +1,78 @@
+﻿using EQUOR.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace EQUOR.Logica_login
+{
+    public class Logica_Login
+    {
+        public Manager EncontrarManager(string correo, string clave)
+        {
+
+            Manager objeto = new Manager();
+
+            using (SqlConnection con = new SqlConnection("Server=DESKTOP-JK7M5A1\\SQLEXPRESS;Database=EQUOR;Integrated Security=True;trustServerCertificate=True"))
+            {
+
+                string query = "select Email,Password from Manager where Email=@pEmail and Password=@pPassword";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@pEmail", correo);
+                cmd.Parameters.AddWithValue("@pPassword", clave);
+
+                cmd.CommandType = CommandType.Text;
+
+                con.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        objeto = new Manager()
+                        {
+                            Email = reader["Email"].ToString(),
+                            Password = reader["Password"].ToString(),
+                            IdRole = (int)reader["IdRole"],
+
+                        };
+                    }
+                }
+            }
+            return objeto;
+        }
+        public Consumer EncontrarConsumer(string correo, string clave)
+        {
+
+            Consumer objeto = new Consumer();
+
+            using (SqlConnection con = new SqlConnection("Server=DESKTOP-JK7M5A1\\SQLEXPRESS;Database=EQUOR;Integrated Security=True;trustServerCertificate=True"))
+            {
+
+                string query = "select Email,Password from Manager where Email=@pEmail and Password=@pPassword";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@pEmail", correo);
+                cmd.Parameters.AddWithValue("@pPassword", clave);
+
+                cmd.CommandType = CommandType.Text;
+
+                con.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        objeto = new Consumer()
+                        {
+                            Email = reader["Email"].ToString(),
+                            Password = reader["Password"].ToString(),
+                            IdRole = (int)reader["IdRole"],
+
+                        };
+                    }
+                }
+            }
+            return objeto;
+        }
+    }
+}
